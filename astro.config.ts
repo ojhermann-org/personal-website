d438318cdbf5bfb1671985bd98a28cc0e1c45ff5
Otto Hermann <ojhermann@gmail.com>
2026-04-02 14:52:45 -0400

    make sure external links open in new tabs

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "astro/config";
+import rehypeExternalLinks from "rehype-external-links";
 
 export default defineConfig({
   output: "static",
   site: "https://otto-hermann.me",
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        { target: "_blank", rel: ["noopener", "noreferrer"] },
+      ],
+    ],
+  },
 });
